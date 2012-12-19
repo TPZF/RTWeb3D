815e27c1a3a5314ec15d7d7f39e8b0ef16393c43
mshubin <maxime.shubin@telespazio.com>
2012-12-19 15:46:41 +0100

    Modify jqueryUI version

--- app/build/build.js
+++ app/build/build.js
@@ -20,7 +20,7 @@
     // Optimizing alredy minified files :( .. TODO exclude
     paths: {
 		"jquery": "../externals/jquery-1.8.2.min",
-		"jquery.ui": "../externals/jquery-ui-1.8.23.custom.min",
+		"jquery.ui": "../externals/jquery-ui-1.9.2.custom.min",
 		"jquery.ui.selectmenu": "../externals/jquery.ui.selectmenu",
 		"underscore-min": "../externals/underscore-min",
 		"jquery.nicescroll.min": "../externals/jquery.nicescroll.min"
