02032c15c806867befc331a892fd5274e516eac2
mshubin <maxime.shubin@telespazio.com>
2013-01-07 11:52:21 +0100

    Replace color code div by explicit rendering in canvas2d(requires "dataType" property in configuration file) Replace gwBaseLayers & gwAdditionalLayers arrays using jQuery .data()

--- app/js/LayerManager.js
+++ app/js/LayerManager.js
@@ -10,15 +10,14 @@ define( [ "jquery.ui", "PickingManager", "ClusterLayer", "MocLayer", "MixLayer",
  * Private variable for module
  */
  
- // The globe
 var globe;
-var gwLayer;
 var navigation;
-var gwBaseLayers = [];
-var gwAdditionalLayers = [];
-var backgroundLayersIcons = [];
-var nbBackgroundLayers = 0;
-var nbAddLayers = 0;
+
+// Necessary for selectmenu initialization
+var backgroundLayersIcons = []; 
+var nbBackgroundLayers = 0; // required because background id is always equal to 0
+
+// GeoJSON data providers
 var dataProviders = {};
 
 // Template generating the additional layer div in sidemenu
@@ -62,7 +61,6 @@ function createLayerFromConf(layer) {
 				strokeColor: rgba
 	});
 
-
 	switch(layer.type){
 		case "healpix":
 			// Add necessary option
@@ -89,6 +87,8 @@ function createLayerFromConf(layer) {
 				var data = callback(gwLayer, layer.data);
 			}
 
+			gwLayer.dataType = layer.dataType || "line";
+
 			if ( layer.pickable )
 				PickingManager.addPickableLayer( gwLayer );
 			break;
@@ -103,6 +103,7 @@ function createLayerFromConf(layer) {
 			
 			options.style = defaultVectorStyle;
 			gwLayer = new GlobWeb.OpenSearchLayer( options );
+			gwLayer.dataType = layer.dataType;
 			PickingManager.addPickableLayer( gwLayer );
 			break;
 		case "Cluster":
@@ -119,12 +120,14 @@ function createLayerFromConf(layer) {
 			options.style = defaultVectorStyle;
 			options.style.iconUrl = layer.iconUrl || "css/images/cluster.png";
 			gwLayer = new ClusterLayer( options );
+			gwLayer.dataType = layer.dataType || "point";
 			break;
 
 		case "Moc":
 			options.style = defaultVectorStyle;
 			options.serviceUrl = layer.serviceUrl;
 			gwLayer = new MocLayer( options );
+			gwLayer.dataType = layer.dataType || "line";
 			break;
 
 		case "Mix":
@@ -176,9 +179,9 @@ function handleDrop(evt) {
 			var rgba = rgb.concat([1]);
 			
 			// Create style
-			var options = {name: name};
+			var options = { name: name };
 			options.style = new GlobWeb.FeatureStyle({ rendererHint: "Basic", iconUrl: "css/images/star.png", fillColor: rgba, strokeColor: rgba });
-			gwLayer = new GlobWeb.VectorLayer( options );
+			var gwLayer = new GlobWeb.VectorLayer( options );
 			gwLayer.deletable = true;
 
 			// Add geoJson layer
@@ -189,7 +192,6 @@ function handleDrop(evt) {
 			PickingManager.addPickableLayer( gwLayer );
 
 			updateScroll();
-			
 		};
 		reader.readAsText(f);
 	});
@@ -211,21 +213,19 @@ function handleDragOver(evt)
 function createHtmlForBackgroundLayer( gwLayer )
 {
 	// Add HTML
-	var currentIndex = gwLayer.id;
 	var currentClass;
 	
-	var layerDiv ='<option value="'+ currentIndex +'" class="">'+ gwLayer.name + '</option>"';
-	
+	var layerDiv ='<option>'+ gwLayer.name + '</option>"';
 	
 	if ( gwLayer.icon )
 	{
 		// Create icon style
 		var sheet = document.createElement('style');
-		sheet.innerHTML = ".backgroundLayer_" + currentIndex + " .ui-selectmenu-item-icon { background: url("+gwLayer.icon+") 0 0 no-repeat; }";
+		sheet.innerHTML = ".backgroundLayer_" + nbBackgroundLayers + " .ui-selectmenu-item-icon { background: url("+gwLayer.icon+") 0 0 no-repeat; }";
 		document.body.appendChild(sheet);
 		
-		backgroundLayersIcons.push( {find: ".backgroundLayer_" + currentIndex} );
-		currentClass = 'backgroundLayer_'+ currentIndex;
+		backgroundLayersIcons.push( {find: ".backgroundLayer_" + nbBackgroundLayers} );
+		currentClass = 'backgroundLayer_'+ nbBackgroundLayers;
 	}
 	else
 	{
@@ -233,8 +233,10 @@ function createHtmlForBackgroundLayer( gwLayer )
 		backgroundLayersIcons.push( {find: ".unknown"} );
 		currentClass = 'unknown';
 	}
-	$(layerDiv).appendTo('#backgroundLayers').addClass(currentClass);
-	
+	$(layerDiv).appendTo('#backgroundLayers')
+				.addClass(currentClass)
+				.data("layer", gwLayer);
+
 	// Set visible layer on top of selector
 	if ( gwLayer.visible() )
 	{
@@ -253,22 +255,88 @@ function updateScroll()
 }
 
 /**
+ *	Generate point legend in canvas 2d
+ *
+ *	@param gwLayer GlobWeb layer
+ *	@param canvas Canvas
+ *	@param imageUrl Image source url used for point texture
+ */
+function generatePointLegend( gwLayer, canvas, imageUrl )
+{
+	var context = canvas.getContext('2d');
+	var icon = new Image();
+	icon.onload = function() {
+		// var width = (icon.width > canvas.width) ? canvas.width : icon.width;
+		// var height = (icon.height > canvas.height) ? canvas.height : icon.height;
+		context.drawImage(icon, 5, 0, 10, 10);
+
+		// colorize icon
+		var data = context.getImageData(0, 0, canvas.width, canvas.height);
+		for (var i = 0, length = data.data.length; i < length; i+=4) {
+			data.data[i] = gwLayer.style.fillColor[0]*255;
+			data.data[i+1] = gwLayer.style.fillColor[1]*255;
+			data.data[i+2] = gwLayer.style.fillColor[2]*255;
+		}
+
+		context.putImageData(data, 0, 0);
+	};
+	icon.src = imageUrl;
+}
+
+/**
+ *	Generate line legend in canvas 2d
+ */
+function generateLineLegend( gwLayer, canvas )
+{
+	var context = canvas.getContext('2d');
+
+	var margin = 2;
+	context.beginPath();
+	context.moveTo(margin, canvas.height - margin);
+	context.lineTo(canvas.width/2 - margin, margin);
+	context.lineTo(canvas.width/2 + margin, canvas.height - margin);
+	context.lineTo(canvas.width - margin, margin);
+	context.lineWidth = 1;
+
+	// set line color
+	context.strokeStyle = GlobWeb.FeatureStyle.fromColorToString(gwLayer.style.fillColor);
+	context.stroke();
+}
+
+/**
  *	Create the Html for addtionnal layers
  */
 function createHtmlForAdditionalLayer( gwLayer )
 {
 	var currentIndex = gwLayer.id;
-	var cssColor = "rgb(1,1,1)";
 
-	if (gwLayer.style)
-	{
-		cssColor = "rgb("+Math.floor(gwLayer.style.fillColor[0]*255)+","+Math.floor(gwLayer.style.fillColor[1]*255)+","+Math.floor(gwLayer.style.fillColor[2]*255)+")";
-	}
-
-	var layerDiv = additionalLayerTemplate( { layer: gwLayer, currentIndex: currentIndex, layerColor: cssColor } );
-
+	var layerDiv = additionalLayerTemplate( { layer: gwLayer, currentIndex: currentIndex } );
 	var $layerDiv = $(layerDiv)
-		.appendTo('#additionalLayers');
+		.appendTo('#additionalLayers')
+		.data("layer", gwLayer);
+
+	var $canvas = $layerDiv.find('.legend');
+	var canvas = $canvas[0];
+
+	if ( gwLayer instanceof GlobWeb.OpenSearchLayer || gwLayer instanceof ClusterLayer || gwLayer instanceof MocLayer || gwLayer instanceof GlobWeb.VectorLayer )
+	{
+		if ( !gwLayer.icon )
+		{
+			if ( gwLayer.dataType == "point")
+			{
+				generatePointLegend(gwLayer, canvas, gwLayer.style.iconUrl);
+			}
+
+			if ( gwLayer.dataType == "line")
+			{
+				generateLineLegend( gwLayer, canvas );
+			}
+		}
+	}
+	else
+	{
+		$canvas.css("display", "none");
+	}
 			
 	// Slider initialisation
 	$('#slider_'+currentIndex).slider({
@@ -340,8 +408,6 @@ function createHtmlForAdditionalLayer( gwLayer )
 		$(this).find('span').toggleClass('ui-icon-empty');
 
 	});
-	
-	nbAddLayers++;
 }
 
 /**
@@ -353,7 +419,6 @@ function addBackgroundLayer ( gwLayer )
 	if ( gwLayer.visible() ) {
 		globe.setBaseImagery( gwLayer );
 	}
-	gwBaseLayers.push( gwLayer );
 	
 	// Add HTML
 	createHtmlForBackgroundLayer( gwLayer );
@@ -365,7 +430,6 @@ function addBackgroundLayer ( gwLayer )
 function addAdditionalLayer ( gwLayer )
 {
 	// Add to engine
-	gwAdditionalLayers.push( gwLayer );
 	globe.addLayer( gwLayer );
 	
 	// Add HTML
@@ -402,14 +466,8 @@ function addAdditionalLayer ( gwLayer )
 	});	
 }
 
-function initGuiEvents ()
+function initToolbarEvents ()
 {
-	
-	// Background selection visibility event
-	$('#backgroundLayers-menu li').click(function(){
-		var layerIndex = parseInt( $(this).index() );
-		globe.setBaseImagery( gwBaseLayers[ layerIndex ] );
-	});
 	
 	// Delete layer event
 	$('#additionalLayers').on("click",'.deleteLayer', function(){
@@ -417,20 +475,18 @@ function initGuiEvents ()
 		$(this).parent().parent().fadeOut(300, function(){
 			$(this).remove();
 		});
-		var layerIndex = parseInt( $(this).parent().parent().index() );
-		var layer = gwAdditionalLayers[ layerIndex ];
+
+		var layer = $(this).parent().parent().data("layer");
 		globe.removeLayer(layer);
 		PickingManager.removePickableLayer( layer );
-		gwAdditionalLayers.splice( layerIndex, 1 );
 
 		updateScroll();
 	});
 
 	// Services event
 	$('#additionalLayers').on("click", ".service", function(){
-		var layerIndex = parseInt( $(this).parent().parent().index() );
-		var layer = gwAdditionalLayers[ layerIndex ];
 
+		var layer = $(this).parent().parent().data("layer");
 		if( $(this).is(':checked') )
 		{
 			ServiceBar.addLayer(layer);
@@ -443,9 +499,8 @@ function initGuiEvents ()
 
 	// ZoomTo event (available for GlobWeb.VectorLayers only)
 	$('#additionalLayers').on("click", ".zoomTo", function(){
-		var layerIndex = parseInt( $(this).parent().parent().index() );
-		var layer = gwAdditionalLayers[ layerIndex ];
 
+		var layer = $(this).parent().parent().data("layer");
 		var sLon = 0;
 		var sLat = 0;
 		var nbPoints = 0;
@@ -498,7 +553,7 @@ function initLayers(layers)
 		if(typeof (layer.pickable) === 'undefined')
 			layer.pickable = true;
 	
-		gwLayer = createLayerFromConf(layer);
+		var gwLayer = createLayerFromConf(layer);
 		if ( gwLayer )
 		{
 			if( layer.background )
@@ -514,10 +569,16 @@ function initLayers(layers)
 	
 	// Init select menu
 	$('select#backgroundLayers').selectmenu({
-		icons: backgroundLayersIcons
+		icons: backgroundLayersIcons,
+		select: function(e)
+		{
+			var index = $(this).data('selectmenu').index();
+			var layer = $(this).children().eq(index).data("layer");
+			globe.setBaseImagery( layer );
+		}
 	});
 	
-	initGuiEvents();
+	initToolbarEvents();
 	
 	// Setup the drag & drop listeners.
 	$('canvas').on('dragover', handleDragOver);
