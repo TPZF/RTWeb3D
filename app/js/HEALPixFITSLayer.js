93b04ad1d58042424580691a045eebc26add2fda
mshubin <maxime.shubin@telespazio.com>
2013-07-23 10:08:36 +0200

    Fix licence

--- app/js/HEALPixFITSLayer.js
+++ app/js/HEALPixFITSLayer.js
@@ -1,21 +1,21 @@
-/***************************************
- * Copyright 2011, 2012 GlobWeb contributors.
- *
- * This file is part of GlobWeb.
- *
- * GlobWeb is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, version 3 of the License, or
- * (at your option) any later version.
- *
- * GlobWeb is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with GlobWeb. If not, see <http://www.gnu.org/licenses/>.
- ***************************************/
+/******************************************************************************* 
+* Copyright 2012, 2013 CNES - CENTRE NATIONAL d'ETUDES SPATIALES 
+* 
+* This file is part of SITools2. 
+* 
+* SITools2 is free software: you can redistribute it and/or modify 
+* it under the terms of the GNU General Public License as published by 
+* the Free Software Foundation, either version 3 of the License, or 
+* (at your option) any later version. 
+* 
+* SITools2 is distributed in the hope that it will be useful, 
+* but WITHOUT ANY WARRANTY; without even the implied warranty of 
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+* GNU General Public License for more details. 
+* 
+* You should have received a copy of the GNU General Public License 
+* along with SITools2. If not, see <http://www.gnu.org/licenses/>. 
+******************************************************************************/ 
 
 define(['gw/Utils', 'gw/HEALPixTiling', 'gw/RasterLayer', 'gw/DynamicImage', 'FitsLoader', 'gw/FitsTilePool', 'DynamicImageView'], 
 	function(Utils, HEALPixTiling, RasterLayer, DynamicImage, FitsLoader, FitsTilePool, DynamicImageView) {
