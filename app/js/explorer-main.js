7556162c512c1a4a11d74b0b9da018b3e39b45fc
Jean-Christophe Malapert <jcmalapert@googlemail.com>
2015-04-14 03:11:35 +0200

    Clean code

--- app/js/explorer-main.js
+++ app/js/explorer-main.js
@@ -1,3 +1,4 @@
+/*global clearInterval: false, clearTimeout: false, document: false, event: false, frames: false, history: false, Image: false, location: false, name: false, navigator: false, Option: false, parent: false, screen: false, setInterval: false, setTimeout: false, window: false, XMLHttpRequest: false, define: false */
 
 /**
  * Configuration for require.js
